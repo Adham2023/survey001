95cdda33cbbb1134f67fae0206aa33fb233e2d32
Adham <adkham2023@yandex.com>
2019-11-09 11:07:47 +0500

    Preview Page is developing

--- src/components/Preview/Preview.vue
+++ src/components/Preview/Preview.vue
@@ -1,120 +1,59 @@
 <template>
-
-  <ol>
-	<li>
-		<h1>Вопрос 1 (Open answer)</h1>
-		<h3 style="color: #642887;">Назовите пожалуйста юридическое название вашего магазина</h3>
-	</li>
-	<li>
-		<h1>Вопрос 2 (Multiple choice single answer)</h1>
-		<h3 style="color: #642887;">К какому типу относится ваш магазин?</h3>
-		<ul>
-			<li>а) Минимаркет</li>
-			<li>б) Супермаркет</li>
-			<li>в) Продуктовый магазин</li>
-			<li>г) Уличный торговец</li>
-			<li>д) Оптовик</li>
-			<li>е) Другое (____________)</li>
-		</ul>
-	</li>
-	<li>
-		<h1>Вопрос 3 (Multiple choice multiple answer)</h1>
-		<h3 style="color: #642887;">Какие категории продуктов вы продаете?</h3>
-		<ul>
-			<li>a) Продукты питания</li>
-			<li>б) Продукты личной гигиены</li>
-			<li>в) Продукты бытовой химии</li>
-			<li>г) Одежда-обувь</li>
-			<li>д) Продукты канцелярии</li>
-			<li>е) все перечисленное</li>
-		</ul>
-	</li>
-	<li>
-		<h1>Вопрос 4 (Yes/No)</h1>
-		<h3 style="color: #642887;">Разрешите ли вы мне снять данные (наличие, цены, остатки) по шоколадной продукции?</h3>
-		<ul>
-			<li>а) ДА</li>
-			<li>б) НЕТ</li>
-		</ul>
-	</li>
-	<li>
-		<h1>Вопрос 5 (Matrix)</h1>
-
-		<table>
-			<thead>
-				<tr>
-					<th>название продукта</th>
-					<th>наличие</th>
-					<th>цена</th>
-					<th>остаток</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Snickers</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Mars</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Twix</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Bounty</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Tubleron</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Lion</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Milkyway</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Nesquik</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				<tr>
-					<td>Metro</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-			</tbody>
-		</table>
-	</li>
-</ol>
-
+  <v-sheet color="blue-grey darken-3">
+	  <v-content style="  padding:0;" >
+		  <v-card height="100%">
+			<v-tabs
+			v-model="tab"
+			width="100%"
+			background-color="blue-grey darken-3"
+			centered
+			dark
+			text
+			icons-and-text
+			>
+			<v-tabs-slider></v-tabs-slider>
+			<v-tab href="#tab-1">
+				Plain View
+				<v-icon>mdi-file-document-box-search-outline</v-icon>
+			</v-tab>
+			<v-tab href="#tab-2">
+				Test View
+				<v-icon>mdi-eye</v-icon>
+			</v-tab>
+			</v-tabs>
+				<v-tabs-items v-model="tab" height="100%" >
+					<v-tab-item
+						value="tab-1"
+						style="height: 80vh; overflow-x: auto; overflow-y: auto; padding:0;"
+					>
+						<PlainView />
+					</v-tab-item>
+					<v-tab-item
+						value="tab-2"
+						style="height: 80vh; overflow-x: auto; overflow-y: auto; padding:0;"
+					>
+						<TestView />
+					</v-tab-item>
+				</v-tabs-items>
+  		</v-card>
+	  </v-content>
+  </v-sheet>
 </template>
-
 <script>
-export default {
-
-}
+import PlainView from './views/PlainView';
+import TestView from './views/TestView';
+  export default {
+	  components: {
+		  PlainView,
+		  TestView
+	  },
+		data () {
+		return {
+			tab: null,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		}
+		},
+  }
 </script>
 
 <style>
